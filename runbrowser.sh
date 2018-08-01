be107c6f4c43de8ff6b6d9142922a4c6f3a197de
RG <rgianni@ed.ac.uk>
2018-08-01 13:11:42 +0000

    use chrom

--- runbrowser.sh
+++ runbrowser.sh
@@ -6,7 +6,8 @@ cd ~/trustball
 
 sleep 8s;
 
-sudo -u pi epiphany-browser -a --profile ~/.config http://localhost:5000 --display=:0 &
+# sudo -u pi epiphany-browser -a --profile ~/.config http://localhost:5000 --display=:0 &
+chromium-browser http://localhost:5000 --start-maximized &
 
 sleep 8s;
 
